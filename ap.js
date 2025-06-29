52cf10e00c78cc7d9a32b2410775883827c1abd5
AzizullahCodes <azizullahcodes@gmail.com>
2025-06-29 21:02:10 +0500

    Initial commit

---
+++ ap.js
@@ -0,0 +1,345 @@
+/* -----------------------------------------
+   JavaScript Q&A Practice assignment 36
+   Author: Azizullah
+   Date: 29-06-2025
+   ----------------------------------------- */
+
+
+/* ========== Question 1 ==========
+Q:  create a pattern by repeat( )
+*/
+// function repeating(){let get = document.getElementById('it').value;
+//     let time = document.getElementById('it2').value;
+//     let result = '';
+//     for(var i = 0; i<=time;i++){result = result + get.repeat(i)+ '\n';}
+//     document.getElementById('ph').innerHTML = result;
+// }
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+function repeating(){let get = document.getElementById('it').value;
+    let time = document.getElementById('it2').value;
+    let result = '';
+    for(var i = 0; i<=time;i++){result = result + get.repeat(i)+ '\n';}
+    document.getElementById('ph').innerHTML = result;
+}
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
+
+
+
+/* ========== Question 1 ==========
+Q:  
+*/
+
+
+
+
+/* ========== Question 2 ==========
+Q: 
+
+*/
